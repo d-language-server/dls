a484ce75ba3ee46b0f5558b6fb76299a39d0994c
Laurent Tréguier <laurent@treguier.org>
2018-03-20 20:19:33 +0100

    Refine dls.util.json and add unit tests

--- source/dls/util/json.d
+++ source/dls/util/json.d
@@ -50,14 +50,79 @@ T convertFromJSON(T)(JSONValue json) if (is(T == class) || is(T == struct))
     return result;
 }
 
+version (unittest)
+{
+    struct TestStruct
+    {
+        uint uinteger;
+        JSONValue json;
+    }
+
+    class TestClass
+    {
+        int integer;
+        float floating;
+        string text;
+        int[] array;
+        string[string] dictionary;
+        TestStruct testStruct;
+    }
+}
+
+unittest
+{
+    const jsonString = `{
+        "integer": 42,
+        "floating": 3.0,
+        "text": "Hello world",
+        "array": [0, 1, 2],
+        "dictionary": {
+            "key1": "value1",
+            "key2": "value2",
+            "key3": "value3"
+        },
+        "testStruct": {
+            "uinteger": 16,
+            "json": {
+                "key": "value"
+            }
+        }
+    }`;
+
+    const testClass = convertFromJSON!TestClass(parseJSON(jsonString));
+    assert(testClass.integer == 42);
+    assert(testClass.floating == 3.0);
+    assert(testClass.text == "Hello world");
+    assert(testClass.array == [0, 1, 2]);
+    const dictionary = ["key1" : "value1", "key2" : "value2", "key3" : "value3"];
+    assert(testClass.dictionary == dictionary);
+    assert(testClass.testStruct.uinteger == 16);
+    assert(testClass.testStruct.json["key"].str == "value");
+}
+
 N convertFromJSON(N : Nullable!T, T)(JSONValue json)
 {
     return (json.type == JSON_TYPE.NULL) ? N() : convertFromJSON!T(json).nullable;
 }
 
+unittest
+{
+    auto json = JSONValue(42);
+    auto result = convertFromJSON!(Nullable!int)(json);
+    assert(!result.isNull && result.get() == json.integer);
+
+    json = JSONValue(null);
+    assert(convertFromJSON!(Nullable!int)(json).isNull);
+}
+
 T convertFromJSON(T : JSONValue)(JSONValue json)
 {
     return json.nullable;
+}
+
+unittest
+{
+    assert(convertFromJSON!JSONValue(JSONValue(42)) == JSONValue(42));
 }
 
 T convertFromJSON(T)(JSONValue json) if (isNumeric!T)
@@ -88,6 +153,20 @@ T convertFromJSON(T)(JSONValue json) if (isNumeric!T)
     }
 }
 
+unittest
+{
+    assert(convertFromJSON!float(JSONValue(3.0)) == 3.0);
+    assert(convertFromJSON!int(JSONValue(42)) == 42);
+    assert(convertFromJSON!uint(JSONValue(42U)) == 42U);
+
+    // quirky JSON cases
+
+    assert(convertFromJSON!int(JSONValue(null)) == 0);
+    assert(convertFromJSON!int(JSONValue(false)) == 0);
+    assert(convertFromJSON!int(JSONValue(true)) == 1);
+    assert(convertFromJSON!int(JSONValue("42")) == 42);
+}
+
 T convertFromJSON(T)(JSONValue json) if (isBoolean!T)
 {
     switch (json.type)
@@ -110,8 +189,26 @@ T convertFromJSON(T)(JSONValue json) if (isBoolean!T)
     }
 }
 
+unittest
+{
+    assert(convertFromJSON!bool(JSONValue(false)) == false);
+    assert(convertFromJSON!bool(JSONValue(true)) == true);
+
+    // quirky JSON cases
+
+    assert(convertFromJSON!bool(JSONValue(null)) == false);
+    assert(convertFromJSON!bool(JSONValue(0.0)) == false);
+    assert(convertFromJSON!bool(JSONValue(0)) == false);
+    assert(convertFromJSON!bool(JSONValue(0U)) == false);
+
+    assert(convertFromJSON!bool(JSONValue(new int[0])) == true);
+    assert(convertFromJSON!bool(JSONValue(3.0)) == true);
+    assert(convertFromJSON!bool(JSONValue(42)) == true);
+    assert(convertFromJSON!bool(JSONValue(42U)) == true);
+}
+
 T convertFromJSON(T)(JSONValue json)
-        if (isSomeChar!T || isSomeString!T || is(T : string))
+        if (isSomeChar!T || isSomeString!T || is(T : string) || is(T : wstring) || is(T : dstring))
 {
     switch (json.type)
     {
@@ -120,13 +217,13 @@ T convertFromJSON(T)(JSONValue json)
             static if (!isSomeChar!T)
             {
     case JSON_TYPE.NULL:
-                return "null";
+                return "null".to!T;
 
     case JSON_TYPE.FALSE:
-                return "false";
+                return "false".to!T;
 
     case JSON_TYPE.TRUE:
-                return "true";
+                return "true".to!T;
             }
 
     case JSON_TYPE.FLOAT:
@@ -151,8 +248,38 @@ T convertFromJSON(T)(JSONValue json)
     }
 }
 
+unittest
+{
+    auto json = JSONValue("Hello");
+    assert(convertFromJSON!string(json) == json.str);
+    assert(convertFromJSON!(char[])(json) == json.str);
+    assert(convertFromJSON!(wchar[])(json) == "Hello"w);
+    assert(convertFromJSON!(dchar[])(json) == "Hello"d);
+
+    // beware of the fact that JSONValue treats chars as integers; this returns "97" and not "a"
+    assert(convertFromJSON!string(JSONValue('a')) != "a");
+    assert(convertFromJSON!string(JSONValue("a")) == "a");
+
+    assert(convertFromJSON!char(JSONValue('a')) == 'a');
+    assert(convertFromJSON!char(JSONValue("a")) == 'a');
+
+    enum TestEnum
+    {
+        hello = "hello",
+        world = "world"
+    }
+
+    assert(convertFromJSON!TestEnum(JSONValue("hello")) == TestEnum.hello);
+
+    // quirky JSON cases
+
+    assert(convertFromJSON!string(JSONValue(null)) == "null");
+    assert(convertFromJSON!string(JSONValue(false)) == "false");
+    assert(convertFromJSON!string(JSONValue(true)) == "true");
+}
+
 T convertFromJSON(T : U[], U)(JSONValue json)
-        if (isArray!T && !isSomeString!T && !is(T : string))
+        if (isArray!T && !isSomeString!T && !is(T : string) && !is(T : wstring) && !is(T : dstring))
 {
     switch (json.type)
     {
@@ -175,9 +302,24 @@ T convertFromJSON(T : U[], U)(JSONValue json)
     }
 }
 
-T convertFromJSON(T : U[string], U)(JSONValue json) if (isAssociativeArray!T)
+unittest
 {
-    U[string] result;
+    assert(convertFromJSON!(int[])(JSONValue([0, 1, 2, 3])) == [0, 1, 2, 3]);
+
+    // quirky JSON cases
+
+    assert(convertFromJSON!(int[])(JSONValue(null)) == []);
+    assert(convertFromJSON!(int[])(JSONValue(false)) == []);
+    assert(convertFromJSON!(int[])(JSONValue(true)) == []);
+    assert(convertFromJSON!(float[])(JSONValue(3.0)) == [3.0]);
+    assert(convertFromJSON!(int[])(JSONValue(42)) == [42]);
+    assert(convertFromJSON!(uint[])(JSONValue(42U)) == [42U]);
+    assert(convertFromJSON!(string[])(JSONValue("Hello")) == ["Hello"]);
+}
+
+T convertFromJSON(T : U[K], U, K)(JSONValue json) if (isAssociativeArray!T)
+{
+    U[K] result;
 
     switch (json.type)
     {
@@ -187,7 +329,7 @@ T convertFromJSON(T : U[string], U)(JSONValue json) if (isAssociativeArray!T)
     case JSON_TYPE.OBJECT:
         foreach (key, value; json.object)
         {
-            result[key] = convertFromJSON!U(value);
+            result[key.to!K] = convertFromJSON!U(value);
         }
 
         break;
@@ -195,7 +337,7 @@ T convertFromJSON(T : U[string], U)(JSONValue json) if (isAssociativeArray!T)
     case JSON_TYPE.ARRAY:
         foreach (key, value; json.array)
         {
-            result[key] = convertFromJSON!U(value);
+            result[key.to!K] = convertFromJSON!U(value);
         }
 
         break;
@@ -207,12 +349,27 @@ T convertFromJSON(T : U[string], U)(JSONValue json) if (isAssociativeArray!T)
     return result;
 }
 
+unittest
+{
+    auto dictionary = ["hello" : 42, "world" : 0];
+    assert(convertFromJSON!(int[string])(JSONValue(dictionary)) == dictionary);
+
+    // quirky JSON cases
+
+    assert(convertFromJSON!(int[string])(JSONValue([16, 42])) == ["0" : 16, "1" : 42]);
+    dictionary.clear();
+    assert(convertFromJSON!(int[string])(JSONValue(null)) == dictionary);
+}
+
 Nullable!JSONValue convertToJSON(T)(T value)
         if ((is(T == class) || is(T == struct)) && !is(T == JSONValue))
 {
-    if (value is null)
+    static if (is(T == class))
     {
-        return Nullable!JSONValue();
+        if (value is null)
+        {
+            return Nullable!JSONValue();
+        }
     }
 
     auto result = JSONValue();
@@ -236,9 +393,40 @@ Nullable!JSONValue convertToJSON(T)(T value)
     return result.nullable;
 }
 
-Nullable!JSONValue convertToJSON(typeof(null))
+unittest
 {
-    return Nullable!JSONValue();
+    auto testClass = new TestClass();
+    testClass.integer = 42;
+    testClass.floating = 3.5;
+    testClass.text = "Hello world";
+    testClass.array = [0, 1, 2];
+    testClass.dictionary = ["key1" : "value1", "key2" : "value2"];
+    testClass.testStruct = TestStruct();
+    testClass.testStruct.uinteger = 16;
+    testClass.testStruct.json = JSONValue(["key1" : "value1", "key2" : "value2"]);
+
+    auto jsonString = `{
+        "integer": 42,
+        "floating": 3.5,
+        "text": "Hello world",
+        "array": [0, 1, 2],
+        "dictionary": {
+            "key1": "value1",
+            "key2": "value2"
+        },
+        "testStruct": {
+            "uinteger": 16,
+            "json": {
+                "key1": "value1",
+                "key2": "value2"
+            }
+        }
+    }`;
+
+    auto json = convertToJSON(testClass);
+    // parseJSON() will parse `uinteger` as a regular integer, meaning that the JSON's are considered equal,
+    // even though technically they are equivalent (16 as int or as uint is technically the same value)
+    assert(json.get().toString() == parseJSON(jsonString).toString());
 }
 
 Nullable!JSONValue convertToJSON(N : Nullable!T, T)(N value)
@@ -246,22 +434,40 @@ Nullable!JSONValue convertToJSON(N : Nullable!T, T)(N value)
     return value.isNull ? Nullable!JSONValue() : convertToJSON!T(value.get());
 }
 
-Nullable!JSONValue convertToJSON(T)(T value) if (is(T == JSONValue))
+unittest
 {
-    return value.nullable;
+    assert(convertToJSON(Nullable!int()) == Nullable!JSONValue());
+    assert(convertToJSON(Nullable!int(42)) == JSONValue(42));
 }
 
 Nullable!JSONValue convertToJSON(T)(T value)
-        if (isNumeric!T || isBoolean!T || isSomeChar!T || isSomeString!T)
+        if ((!is(T == class) && !is(T == struct)) || is(T == JSONValue))
 {
     return JSONValue(value).nullable;
+}
+
+unittest
+{
+    assert(convertToJSON(3.0) == JSONValue(3.0));
+    assert(convertToJSON(42) == JSONValue(42));
+    assert(convertToJSON(42U) == JSONValue(42U));
+    assert(convertToJSON(false) == JSONValue(false));
+    assert(convertToJSON(true) == JSONValue(true));
+    assert(convertToJSON('a') == JSONValue('a'));
+    assert(convertToJSON("Hello world") == JSONValue("Hello world"));
 }
 
 Nullable!JSONValue convertToJSON(T : U[], U)(T value)
         if (isArray!T && !isSomeString!T)
 {
     return JSONValue(value.map!((item) => convertToJSON(item))()
-            .map!((json) => json.isNull ? JSONValue(null) : json.get())().array).nullable;
+            .map!((json) => json.isNull ? JSONValue(null) : json)().array).nullable;
+}
+
+unittest
+{
+    assert(convertToJSON([0, 1, 2]) == JSONValue([0, 1, 2]));
+    assert(convertToJSON(["hello", "world"]) == JSONValue(["hello", "world"]));
 }
 
 Nullable!JSONValue convertToJSON(T : U[string], U)(T value)
@@ -272,18 +478,18 @@ Nullable!JSONValue convertToJSON(T : U[string], U)(T value)
     foreach (key; value.keys)
     {
         auto json = convertToJSON(value[key]);
-
-        if (json.isNull())
-        {
-            result[key] = null;
-        }
-        else
-        {
-            result[key] = json.get();
-        }
+        result[key] = json.isNull ? JSONValue(null) : json;
     }
 
     return result.nullable;
+}
+
+unittest
+{
+    auto json = convertToJSON(["hello" : 16, "world" : 42]);
+    assert(!json.isNull);
+    assert(json["hello"].integer == 16);
+    assert(json["world"].integer == 42);
 }
 
 /++
@@ -295,4 +501,12 @@ private auto normalizeMemberName(string name)
     import std.string : endsWith;
 
     return name.endsWith('_') ? name[0 .. $ - 1] : name;
+}
+
+unittest
+{
+    assert(normalizeMemberName("hello") == "hello");
+    assert(normalizeMemberName("hello_") == "hello");
+    assert(normalizeMemberName("_hello") == "_hello");
+    assert(normalizeMemberName("hel_lo") == "hel_lo");
 }
