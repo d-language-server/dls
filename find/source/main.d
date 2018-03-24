1b04628632ac19a821515bdb98348677eeee0cea
Laurent Tréguier <laurent@treguier.org>
2018-03-24 18:26:19 +0100

    Output current dls directory instead of fetching latest

--- find/source/main.d
+++ find/source/main.d
@@ -1,17 +1,9 @@
 int main()
 {
-    import dub.dependency : Dependency;
-    import dub.dub : Dub, FetchOptions;
-    import dub.internal.vibecompat.core.log : LogLevel, setLogLevel;
+    import std.file : thisExePath;
+    import std.path : buildNormalizedPath, dirName;
     import std.stdio : stdout;
 
-    setLogLevel(LogLevel.none);
-
-    auto dub = new Dub();
-    FetchOptions fetchOpts;
-    fetchOpts |= FetchOptions.forceBranchUpgrade;
-    stdout.write(dub.fetch("dls", Dependency(">=0.0.0"),
-            dub.defaultPlacementLocation, fetchOpts).path);
-
+    stdout.write(thisExePath().dirName.dirName);
     return 0;
 }
