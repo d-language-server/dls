ad3c74b187acc8624fc05ef7b5bf86ba299fecb1
Laurent Tréguier <laurent@treguier.org>
2018-05-12 23:50:55 +0200

    Remove dls:find

--- find/source/main.d
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    import std.file : thisExePath;
-    import std.path : dirName;
-    import std.stdio : stdout;
-
-    stdout.write(thisExePath().dirName.dirName);
-    return 0;
-}
