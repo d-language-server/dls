3c275d6da1da4a116b67cccc0cc9124926cc9c37
Laurent Tréguier <laurent@treguier.org>
2018-06-08 21:20:33 +0200

    Re-organize jsonrpc module

--- source/dls/protocol/jsonrpc.d
+++ source/dls/protocol/jsonrpc.d
@@ -6,89 +6,6 @@ import std.typecons : Nullable, Tuple, nullable, tuple;
 private enum jsonrpcVersion = "2.0";
 private enum eol = "\r\n";
 
-private void send(T : Message)(T m)
-{
-    import dls.util.json : convertToJSON;
-    import std.conv : to;
-    import std.stdio : stdout;
-    import std.utf : toUTF8;
-
-    auto message = convertToJSON(m);
-    auto messageString = message.get().toString();
-
-    synchronized
-    {
-        foreach (chunk; ["Content-Length: ", messageString.length.to!string,
-                eol, eol, messageString])
-        {
-            stdout.rawWrite(chunk.toUTF8());
-        }
-
-        stdout.flush();
-    }
-}
-
-static void sendError(ErrorCodes error, RequestMessage request, JSONValue data)
-{
-    if (request !is null)
-    {
-        send(request.id, Nullable!JSONValue(), ResponseError.fromErrorCode(error, data).nullable);
-    }
-}
-
-/++ Sends a request or a notification message. +/
-static string send(string method, Nullable!JSONValue params = Nullable!JSONValue())
-{
-    import dls.protocol.handlers : hasRegisteredHandler, pushHandler;
-    import std.uuid : randomUUID;
-
-    if (hasRegisteredHandler(method))
-    {
-        auto id = "dls-" ~ randomUUID().toString();
-        pushHandler(id, method);
-        send!RequestMessage(JSONValue(id), method, params, Nullable!ResponseError());
-        return id;
-    }
-
-    send!NotificationMessage(JSONValue(), method, params, Nullable!ResponseError());
-    return null;
-}
-
-static string send(T)(string method, T params) if (!is(T : Nullable!JSONValue))
-{
-    import dls.util.json : convertToJSON;
-
-    return send(method, convertToJSON(params).nullable);
-}
-
-/++ Sends a response message. +/
-static void send(JSONValue id, Nullable!JSONValue result,
-        Nullable!ResponseError error = Nullable!ResponseError())
-{
-    send!ResponseMessage(id, null, result, error);
-}
-
-private static void send(T : Message)(JSONValue id, string method,
-        Nullable!JSONValue payload, Nullable!ResponseError error)
-{
-    import std.meta : AliasSeq;
-    import std.traits : select;
-
-    auto message = new T();
-
-    __traits(getMember, message, select!(__traits(hasMember, T, "params"))("params", "result")) = payload;
-
-    foreach (member; AliasSeq!("id", "method", "error"))
-    {
-        static if (__traits(hasMember, T, member))
-        {
-            mixin("message." ~ member ~ " = " ~ member ~ ";");
-        }
-    }
-
-    send(message);
-}
-
 abstract class Message
 {
     string jsonrpc = jsonrpcVersion;
@@ -151,4 +68,87 @@ enum ErrorCodes : Tuple!(int, string)
 class CancelParams
 {
     JSONValue id;
+}
+
+void sendError(ErrorCodes error, RequestMessage request, JSONValue data)
+{
+    if (request !is null)
+    {
+        send(request.id, Nullable!JSONValue(), ResponseError.fromErrorCode(error, data).nullable);
+    }
+}
+
+/++ Sends a request or a notification message. +/
+string send(string method, Nullable!JSONValue params = Nullable!JSONValue())
+{
+    import dls.protocol.handlers : hasRegisteredHandler, pushHandler;
+    import std.uuid : randomUUID;
+
+    if (hasRegisteredHandler(method))
+    {
+        auto id = "dls-" ~ randomUUID().toString();
+        pushHandler(id, method);
+        send!RequestMessage(JSONValue(id), method, params, Nullable!ResponseError());
+        return id;
+    }
+
+    send!NotificationMessage(JSONValue(), method, params, Nullable!ResponseError());
+    return null;
+}
+
+string send(T)(string method, T params) if (!is(T : Nullable!JSONValue))
+{
+    import dls.util.json : convertToJSON;
+
+    return send(method, convertToJSON(params).nullable);
+}
+
+/++ Sends a response message. +/
+void send(JSONValue id, Nullable!JSONValue result,
+        Nullable!ResponseError error = Nullable!ResponseError())
+{
+    send!ResponseMessage(id, null, result, error);
+}
+
+private void send(T : Message)(JSONValue id, string method,
+        Nullable!JSONValue payload, Nullable!ResponseError error)
+{
+    import std.meta : AliasSeq;
+    import std.traits : select;
+
+    auto message = new T();
+
+    __traits(getMember, message, select!(__traits(hasMember, T, "params"))("params", "result")) = payload;
+
+    foreach (member; AliasSeq!("id", "method", "error"))
+    {
+        static if (__traits(hasMember, T, member))
+        {
+            mixin("message." ~ member ~ " = " ~ member ~ ";");
+        }
+    }
+
+    send(message);
+}
+
+private void send(T : Message)(T m)
+{
+    import dls.util.json : convertToJSON;
+    import std.conv : to;
+    import std.stdio : stdout;
+    import std.utf : toUTF8;
+
+    auto message = convertToJSON(m);
+    auto messageString = message.get().toString();
+
+    synchronized
+    {
+        foreach (chunk; ["Content-Length: ", messageString.length.to!string,
+                eol, eol, messageString])
+        {
+            stdout.rawWrite(chunk.toUTF8());
+        }
+
+        stdout.flush();
+    }
 }
