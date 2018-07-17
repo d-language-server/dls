ee76908182f1b38425448534edef9cfdd7c1d8a9
Laurent Tréguier <laurent@treguier.org>
2018-07-17 20:02:20 +0200

    Use enums for method names

--- source/dls/protocol/messages/methods.d
+++ source/dls/protocol/messages/methods.d
@@ -1,98 +1,84 @@
 module dls.protocol.messages.methods;
 
 //dfmt off
-final class General
+enum General : string
 {
-    @disable this() {}
-
-    static immutable initialize    = "initialize";
-    static immutable initialized   = "initialized";
-    static immutable shutdown      = "shutdown";
-    static immutable exit          = "exit";
-    static immutable cancelRequest = "$/cancelRequest";
+    initialize    = "initialize",
+    initialized   = "initialized",
+    shutdown      = "shutdown",
+    exit          = "exit",
+    cancelRequest = "$/cancelRequest",
 }
 
-final class Window
+enum Window : string
 {
-    @disable this() {}
-
-    static immutable showMessage           = "window/showMessage";
-    static immutable showMessageRequest    = "window/showMessageRequest";
-    static immutable logMessage            = "window/logMessage";
+    showMessage           = "window/showMessage",
+    showMessageRequest    = "window/showMessageRequest",
+    logMessage            = "window/logMessage",
 }
 
-final class Telemetry
+enum Telemetry : string
 {
-    @disable this() {}
-
-    static immutable event = "telemetry/event";
+    event = "telemetry/event",
 }
 
-final class Client
+enum Client : string
 {
-    @disable this() {}
-
-    static immutable registerCapability    = "client/registerCapability";
-    static immutable unregisterCapability  = "client/unregisterCapability";
+    registerCapability    = "client/registerCapability",
+    unregisterCapability  = "client/unregisterCapability",
 }
 
-final class Workspace
+enum Workspace : string
 {
-    @disable this() {}
-
-    static immutable workspaceFolders          = "workspace/workspaceFolders";
-    static immutable didChangeWorkspaceFolders = "workspace/didChangeWorkspaceFolders";
-    static immutable configuration             = "workspace/configuration";
-    static immutable didChangeWatchedFiles     = "workspace/didChangeWatchedFiles";
-    static immutable symbol                    = "workspace/symbol";
-    static immutable executeCommand            = "workspace/executeCommand";
-    static immutable applyEdit                 = "workspace/applyEdit";
+    workspaceFolders          = "workspace/workspaceFolders",
+    didChangeWorkspaceFolders = "workspace/didChangeWorkspaceFolders",
+    configuration             = "workspace/configuration",
+    didChangeWatchedFiles     = "workspace/didChangeWatchedFiles",
+    symbol                    = "workspace/symbol",
+    executeCommand            = "workspace/executeCommand",
+    applyEdit                 = "workspace/applyEdit",
 }
 
-final class TextDocument
+enum TextDocument : string
 {
-    @disable this() {}
-
-    static immutable didOpen               = "textDocument/didOpen";
-    static immutable didChange             = "textDocument/didChange";
-    static immutable willSave              = "textDocument/willSave";
-    static immutable willSaveWaitUntil     = "textDocument/willSaveWaitUntil";
-    static immutable didSave               = "textDocument/didSave";
-    static immutable didClose              = "textDocument/didClose";
-    static immutable publishDiagnostics    = "textDocument/publishDiagnostics";
-    static immutable completion            = "textDocument/completion";
-    static immutable completionResolve     = "completionItem/resolve";
-    static immutable hover                 = "textDocument/hover";
-    static immutable signatureHelp         = "textDocument/signatureHelp";
-    static immutable definition            = "textDocument/definition";
-    static immutable typeDefinition        = "textDocument/typeDefinition";
-    static immutable implementation        = "textDocument/implementation";
-    static immutable references            = "textDocument/references";
-    static immutable documentHighlight     = "textDocument/documentHighlight";
-    static immutable documentSymbol        = "textDocument/documentSymbol";
-    static immutable codeAction            = "textDocument/codeAction";
-    static immutable codeLens              = "textDocument/codeLens";
-    static immutable codeLensResolve       = "codeLens/resolve";
-    static immutable documentLink          = "textDocument/documentLink";
-    static immutable documentLinkResolve   = "documentLink/resolve";
-    static immutable documentColor         = "textDocument/documentColor";
-    static immutable colorPresentation     = "textDocument/colorPresentation";
-    static immutable formatting            = "textDocument/formatting";
-    static immutable rangeFormatting       = "textDocument/rangeFormatting";
-    static immutable onTypeFormatting      = "textDocument/onTypeFormatting";
-    static immutable rename                = "textDocument/rename";
+    didOpen               = "textDocument/didOpen",
+    didChange             = "textDocument/didChange",
+    willSave              = "textDocument/willSave",
+    willSaveWaitUntil     = "textDocument/willSaveWaitUntil",
+    didSave               = "textDocument/didSave",
+    didClose              = "textDocument/didClose",
+    publishDiagnostics    = "textDocument/publishDiagnostics",
+    completion            = "textDocument/completion",
+    completionResolve     = "completionItem/resolve",
+    hover                 = "textDocument/hover",
+    signatureHelp         = "textDocument/signatureHelp",
+    definition            = "textDocument/definition",
+    typeDefinition        = "textDocument/typeDefinition",
+    implementation        = "textDocument/implementation",
+    references            = "textDocument/references",
+    documentHighlight     = "textDocument/documentHighlight",
+    documentSymbol        = "textDocument/documentSymbol",
+    codeAction            = "textDocument/codeAction",
+    codeLens              = "textDocument/codeLens",
+    codeLensResolve       = "codeLens/resolve",
+    documentLink          = "textDocument/documentLink",
+    documentLinkResolve   = "documentLink/resolve",
+    documentColor         = "textDocument/documentColor",
+    colorPresentation     = "textDocument/colorPresentation",
+    formatting            = "textDocument/formatting",
+    rangeFormatting       = "textDocument/rangeFormatting",
+    onTypeFormatting      = "textDocument/onTypeFormatting",
+    rename                = "textDocument/rename",
 }
 
-final class Dls
+enum Dls : string
 {
-    @disable this() {}
-
-    static immutable upgradeDls_start          = "$/dls.upgradeDls.start";
-    static immutable upgradeDls_stop           = "$/dls.upgradeDls.stop";
-    static immutable upgradeDls_totalSize      = "$/dls.upgradeDls.totalSize";
-    static immutable upgradeDls_currentSize    = "$/dls.upgradeDls.currentSize";
-    static immutable upgradeDls_extract        = "$/dls.upgradeDls.extract";
-    static immutable upgradeSelections_start   = "$/dls.upgradeSelections.start";
-    static immutable upgradeSelections_stop    = "$/dls.upgradeSelections.stop";
+    upgradeDls_start          = "$/dls.upgradeDls.start",
+    upgradeDls_stop           = "$/dls.upgradeDls.stop",
+    upgradeDls_totalSize      = "$/dls.upgradeDls.totalSize",
+    upgradeDls_currentSize    = "$/dls.upgradeDls.currentSize",
+    upgradeDls_extract        = "$/dls.upgradeDls.extract",
+    upgradeSelections_start   = "$/dls.upgradeSelections.start",
+    upgradeSelections_stop    = "$/dls.upgradeSelections.stop",
 }
 //dfmt on
