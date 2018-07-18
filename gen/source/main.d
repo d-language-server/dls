6af98b0ec13b2d3b47a09e42763444b83b53ede6
Laurent Tréguier <laurent@treguier.org>
2018-07-18 19:00:34 +0200

    Use thisExePath instead of __FILE__

--- gen/source/main.d
+++ gen/source/main.d
@@ -1,13 +1,13 @@
 void main()
 {
     import std.algorithm : sort;
-    import std.file : append, readText, write;
+    import std.file : append, readText, thisExePath, write;
     import std.format : format;
     import std.json : parseJSON;
     import std.path : buildNormalizedPath;
     import std.range : replace;
 
-    immutable i18nDir = buildNormalizedPath(__FILE__, "..", "..", "..", "i18n");
+    immutable i18nDir = buildNormalizedPath(thisExePath, "..", "..", "i18n");
     immutable translationsPath = buildNormalizedPath(i18nDir, "data", "translations.json");
     immutable trModulePath = buildNormalizedPath(i18nDir, "source", "dls", "util", "constants.d");
     auto translations = parseJSON(readText(translationsPath));
@@ -15,9 +15,7 @@ void main()
     write(trModulePath, "module dls.util.constants;\n\n");
     append(trModulePath, "enum Tr : string\n{\n");
 
-    auto keys = sort(translations.object.keys);
-
-    foreach (key; keys)
+    foreach (key; sort(translations.object.keys))
     {
         append(trModulePath, format!"%s = \"%s\"%s\n"(key.replace(".", "_"), key, ","));
     }
