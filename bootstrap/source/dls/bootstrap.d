2d72c2213bc5e5b1708c5215e75ad7af1561f4a5
Laurent Tréguier <laurent@treguier.org>
2018-07-20 23:36:15 +0200

    Better comply to GPLv3

--- bootstrap/source/dls/bootstrap.d
+++ bootstrap/source/dls/bootstrap.d
@@ -1,3 +1,23 @@
+/*
+ *Copyright (C) 2018 Laurent Tréguier
+ *
+ *This file is part of DLS.
+ *
+ *DLS is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *DLS is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with DLS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 module dls.bootstrap;
 
 import std.file : exists, isFile, mkdirRecurse, remove;
