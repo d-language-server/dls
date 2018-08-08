02418aee762121cc20a725bc6773354541e0b87c
Laurent Tréguier <laurent@treguier.org>
2018-08-08 22:30:24 +0200

    Add dfmt formatting check

---
+++ .travis-check-dfmt.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+modifiedFiles=$(git status --short | wc -l)
+
+for sourceFile in $(find -name '*.d')
+do
+    dfmt --inplace $sourceFile
+done
+
+if [[ $(git status --short | wc -l) != $modifiedFiles ]]
+then
+    exit 1
+fi
+
+exit 0
