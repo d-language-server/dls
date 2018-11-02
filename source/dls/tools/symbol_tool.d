6a59cad93471379eab63683082573d869bd6e92f
Laurent Tréguier <laurent@treguier.org>
2018-11-02 19:42:26 +0100

    Fix crash with document symbols

--- source/dls/tools/symbol_tool.d
+++ source/dls/tools/symbol_tool.d
@@ -1250,7 +1250,7 @@ private class SymbolVisitor(SymbolType) : ASTVisitor
     override void visit(const Invariant dec)
     {
         tryInsert("invariant", SymbolKind.function_, getRange(dec),
-                dec.blockStatement.endLocation);
+                dec.blockStatement is null ? 0 : dec.blockStatement.endLocation);
     }
 
     override void visit(const VariableDeclaration dec)
@@ -1303,16 +1303,19 @@ private class SymbolVisitor(SymbolType) : ASTVisitor
 
     private size_t getFunctionEndLocation(A : ASTNode)(const A dec)
     {
-        size_t endLocation;
-
         if (dec.functionBody !is null)
         {
-            endLocation = dec.functionBody.bodyStatement !is null
-                ? dec.functionBody.bodyStatement.blockStatement.endLocation
+            if (dec.functionBody.bodyStatement !is null)
+            {
+                return dec.functionBody.bodyStatement.blockStatement is null ? 0
+                    : dec.functionBody.bodyStatement.blockStatement.endLocation;
+            }
+
+            return dec.functionBody.blockStatement is null ? 0
                 : dec.functionBody.blockStatement.endLocation;
         }
 
-        return endLocation;
+        return 0;
     }
 
     private void visitSymbol(A : ASTNode)(const A dec, SymbolKind kind,
