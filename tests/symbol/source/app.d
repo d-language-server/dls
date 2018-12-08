81a068b252bee64c43c9bf28ceb1278f35a707e7
Laurent Tréguier <laurent@treguier.org>
2018-12-08 12:17:28 +0100

    Add basic symbol search tests

---
+++ tests/symbol/source/app.d
@@ -0,0 +1,30 @@
+import std.stdio;
+
+void main()
+{
+	auto var = 42;
+	writeln("Edit source/app.d to start your project.");
+}
+
+class Class
+{
+	int number;
+
+	union
+	{
+		int anonymous;
+		string symbol;
+	}
+}
+
+struct Struct
+{
+	string name;
+}
+
+enum Enum
+{
+	foo,
+	bar,
+	baz
+}
